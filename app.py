0fed0b674e15aa6e15560049dd55a11f1308f75b
Deep <deepvasoya87@gmail.com>
2025-05-05 19:23:57 +0530

    fix bugs

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ def setup_driver():
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-notifications")
 
+    options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome/chrome"
+
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
