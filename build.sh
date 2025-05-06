9e009be90c5c4504b97b061e34316461a4a29de6
Deep <deepvasoya87@gmail.com>
2025-05-06 14:16:25 +0530

    fix bugs

--- build.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p .render/chrome
-cd .render/chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-ar x google-chrome-stable_current_amd64.deb
-tar -xvf data.tar.xz
